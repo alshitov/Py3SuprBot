9b0cf80937e87eb81d78d3d29a26c20ea4bd379c
ympo2oo2 <joj.lean@yandex.ru>
2018-09-15 12:36:30 +0300

    Edited main window(is now resizable, added layouts, scroll area for items)

--- sup_app.py
+++ sup_app.py
@@ -1,68 +1,76 @@
 import os
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
 
-class SupremeApp(QtGui.QWidget):
+class SupremeApp(QWidget):
 
     def __init__(self):
         super(SupremeApp, self).__init__()
+        horizbox = QHBoxLayout()
+        vertbox = QVBoxLayout()
 
         #             buttons               #
-        self.init_user_btn = QtGui.QPushButton('Initialize User', self)
-        self.cart_btn = QtGui.QPushButton('Cart', self)
-        self.sizing_btn = QtGui.QPushButton('Sizing', self)
-        self.bot_help_btn = QtGui.QPushButton('Bot Help', self)
-        self.start_btn = QtGui.QPushButton('Start Bot', self)
+        self.init_user_btn = QPushButton('Initialize User', self)
+        self.cart_btn = QPushButton('Cart', self)
+        self.sizing_btn = QPushButton('Sizing', self)
+        self.bot_help_btn = QPushButton('Bot Help', self)
+        self.start_btn = QPushButton('Start Bot', self)
 
-        self.init_user_btn.move(4, 540)
-        self.cart_btn.move(207, 540)
-        self.sizing_btn.move(410, 540)
-        self.bot_help_btn.move(613, 540)
-        self.start_btn.move(816, 540)
+        horizbox.addWidget(self.init_user_btn)
+        horizbox.addWidget(self.cart_btn)
+        horizbox.addWidget(self.sizing_btn)
+        horizbox.addWidget(self.bot_help_btn)
+        horizbox.addWidget(self.start_btn)
 
-        self.init_user_btn.setFixedSize(200, 30)
-        self.cart_btn.setFixedSize(200, 30)
-        self.sizing_btn.setFixedSize(200, 30)
-        self.bot_help_btn.setFixedSize(200, 30)
-        self.start_btn.setFixedSize(200, 30)
+        #            items field            #
+        self.field_layout = QGridLayout()
 
-        #           set stylesheet          #
-        # self.setStyleSheet("""
-        #     QWidget {
-        #         background-image: url("supr.png");
-        #     }
-        #
-        #     QPushButton {
-        #         background-color: red;
-        #         border-radius: 2px;
-        #     }
-        # """)
+        #            test adding items to table    #
+        cell_1 = QLabel(self)
+        pixmap = QPixmap('TempPNGS/' + 'new01.png')
+        cell_1.setPixmap(pixmap)
+        self.field_layout.addWidget(cell_1, 0, 0)
+        cell_2 = QLabel(self)
+        pixmap = QPixmap('TempPNGS/' + 'new02.png')
+        cell_2.setPixmap(pixmap)
+        self.field_layout.addWidget(cell_2, 0, 1)
+
+        area = QWidget()
+        area.setLayout(self.field_layout)
+        self.items_field = QScrollArea()
+        self.items_field.setWidget(area)
+        self.items_field.show()
+        vertbox.addWidget(self.items_field)
+        vertbox.addLayout(horizbox)
 
         #           main window           #
-        self.setGeometry(2500, 200, 1024, 576) # 169
-        self.setFixedSize(1024, 576)
+        self.setGeometry(2500, 100, 1040, 875)
+        self.setLayout(vertbox)
+        self.setFixedWidth(1040)
         self.setWindowTitle('PySupBot')
         self.scriptDir = os.path.dirname(os.path.realpath(__file__))
-        self.setWindowIcon(QtGui.QIcon(self.scriptDir + os.path.sep + 'supr.png'))
+        self.setWindowIcon(QIcon(self.scriptDir + os.path.sep + 'supr.png'))
         self.show()
-        self.create_table()
+        # self.create_table()
 
-
+    #        edit method!          #
     def create_table(self):
         images_path = self.scriptDir + '/TempPNGS'
         images = os.listdir(images_path)
 
         for idx, img in enumerate(images):
-            cell = QtGui.QLabel(self)
-            cell.setGeometry(10 + (idx-1)*210, 5, 210, 210)
-            pixmap = QtGui.QPixmap('TempPNGS/' + 'new' + str(0) + str(idx) + '.png')
+            cell = QLabel(self)
+            cell.setGeometry(10 + (idx - 1) * 210, 5, 210, 210)
+            pixmap = QPixmap('TempPNGS/' + 'new' + str(0) + str(idx) + '.png')
             cell.setPixmap(pixmap)
-            cell.show()
+            self.field_layout.addWidget(cell, idx, idx)
+
 
 
 def mainApp():
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     GUI = SupremeApp()
     sys.exit(app.exec_())
 
