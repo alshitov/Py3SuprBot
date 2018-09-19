029414a8df85ce044c28483ca04e4514d77388fd
ympo2oo2 <joj.lean@yandex.ru>
2018-09-20 00:09:05 +0300

    Dynamically adding separate tables with items, set headers(missing measures)

--- sizing_window.py
+++ sizing_window.py
@@ -15,26 +15,23 @@ class SizingHelpModalWindow(QDialog):
         #       information fields      #
         self.logo_label = QLabel()
         self.current_season_label = QLabel()
-        self.sizing_table = QTableWidget()
+
+        #      scroll area for tables    #
+        self.tables_layout = QVBoxLayout()
+        self.tables_layout.setContentsMargins(5, 5, 5, 5)
+
+        self.create_tables()
+
+        self.area = QWidget()
+        self.area.setLayout(self.tables_layout)
+        self.tables_area = QScrollArea()
+        self.tables_area.setWidget(self.area)
+        self.tables_area.show()
 
         #       placing widgets         #
         self.layout.addWidget(self.logo_label)
         self.layout.addWidget(self.current_season_label)
-        self.layout.addWidget(self.sizing_table)
-
-        #      configuring table        #
-        self.sizing_table.setColumnCount(5)
-        self.sizing_table.setRowCount(50)
-        self.sizing_table.verticalHeader().hide()
-
-        headers = ['Item / Measure', 'S', 'M', 'L', 'XL']
-
-        self.sizing_table.setHorizontalHeaderLabels(headers)
-        self.sizing_table.setColumnWidth(0, 335)
-        self.sizing_table.setColumnWidth(1, 80)
-        self.sizing_table.setColumnWidth(2, 80)
-        self.sizing_table.setColumnWidth(3, 80)
-        self.sizing_table.setColumnWidth(4, 80)
+        self.layout.addWidget(self.tables_area)
 
         #           alignment           #
         self.logo_label.setAlignment(Qt.AlignHCenter)
@@ -44,15 +41,34 @@ class SizingHelpModalWindow(QDialog):
         pixmap = QPixmap('logo.png')
         self.logo_label.setPixmap(pixmap)
         self.current_season_label.setText('Fall/Winter 2018 Sizing')
-        self.get_contents()
 
         self.dialog_window.setLayout(self.layout)
-        self.dialog_window.setFixedSize(700, 450)
+        self.dialog_window.setFixedSize(720, 450)
         self.dialog_window.setWindowTitle("Supreme Sizing")
         self.dialog_window.setModal(True)
         self.dialog_window.exec_()
 
 
-    def get_contents(self):
-        contents = sizing_parser.parse_table()
-        print(contents)
+    def create_tables(self):
+        #    getting contents for tables    #
+        self.table_contents = sizing_parser.parse_table()
+
+        #      creating separate table      #
+        for index, item in enumerate(self.table_contents):
+            self.sizing_table = QTableWidget()
+
+            self.sizing_table.setFixedSize(665, 150)
+            self.sizing_table.horizontalHeader().setResizeMode(0, QHeaderView.Stretch)
+            self.tables_layout.addWidget(self.sizing_table)
+
+            self.sizing_table.setColumnCount(5)
+            self.sizing_table.setRowCount(4)
+            self.sizing_table.verticalHeader().hide()
+
+            headers = self.table_contents[index]['name']
+            self.sizing_table.setHorizontalHeaderLabels(headers)
+            self.sizing_table.setColumnWidth(0, 340)
+            self.sizing_table.setColumnWidth(1, 80)
+            self.sizing_table.setColumnWidth(2, 80)
+            self.sizing_table.setColumnWidth(3, 80)
+            self.sizing_table.setColumnWidth(4, 80)
