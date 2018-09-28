ba9a97d248a3d76ec0a4cb92a790292ce2b80e86
ympo2oo2 <joj.lean@yandex.ru>
2018-09-28 18:39:07 +0300

    United parsers in one class

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from random import choice
 
 
 class Parser():
@@ -7,8 +8,8 @@ class Parser():
         pass
 
 
-    def get_html(self, url, headers=None, proxies=None, cookies=None):
-        request = requests.get(url)
+    def get_html(self, url, headers=None, proxies=None):
+        request = requests.get(url, headers=headers, proxies=proxies)
         return request.text
 
 
@@ -85,3 +86,80 @@ class Parser():
         current_title = article.find('h2').text
 
         return [self.objects, current_title]
+
+
+    def parse_main_window_content(self):
+        # first, refreshing proxy lists
+        self.parse_http_proxies()
+        self.parse_ssl_https_proxies()
+        # target url
+        url = 'https://www.supremecommunity.com'
+        # reading (already refreshed) proxies and user-agents for request
+        agents_list = open('useragents.txt').read().split('\n')
+        http_proxies = open('http_proxies.txt').read().split('\n')
+        https_proxies = open('https_proxies.txt').read().split('\n')
+        # setting up proxy dict
+        proxies = {
+            'http': choice(http_proxies),
+            'https': choice(https_proxies)
+        }
+        # headers like from normal browser
+        headers = {
+            "Authority": "www.supremecommunity.com",
+            "Method": "GET",
+            "Path": "/",
+            "Scheme": "https",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip,deflate,br",
+            "Accept-Language": "en-US,en;q=0.9,ru;q=0.8",
+            "Cache-Control": "max-age=0",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": choice(agents_list)
+        }
+
+        try:
+            # get to page with droplists
+            html = self.get_html(url + '/season/latest/droplists', headers, proxies)
+
+            # init bs object
+            soup = BeautifulSoup(html, 'html.parser')
+
+            # find latest(future) droplist link
+            latest = soup.find('a', class_='block').get('href')
+
+            # forming latest(future) droplist link
+            link_to_items = url + latest
+
+            # get to latest(future) froplist
+            html = self.get_html(link_to_items, headers, proxies)
+
+            # re-init bs object with new content
+            soup = BeautifulSoup(html, 'html.parser')
+
+            # find items divs which contain items type, name, picture (and price)
+            items_divs = soup.select('div.masonry__item')
+
+            # forming dicrtionaries with items description
+            drop = []
+            for div in items_divs:
+                drop_elem = {}
+                # e.g.:<div class="masonry__item col-sm-4 col-xs-6 filter-sweatshirts"...>
+                drop_elem['type'] = div.get('class')[3][7:]
+
+                img = div.find('img')
+                img_alt_split = img.get('alt').split(' - ')
+
+                drop_elem['name'] = img_alt_split[0].strip()
+                drop_elem['description'] = img_alt_split[1].strip()
+                drop_elem['price'] = div.select_one('span.label-price').text.strip()
+                drop_elem['image'] = url + str(img.get('src'))  # links to images, download later!
+
+                drop.append(drop_elem)
+
+            for i in drop:
+                print(i)
+
+        except requests.exceptions.ConnectionError:
+            print('Error! SSL: {}, HTTP: {}'.format(proxies['https'], proxies['http']))
+            # if error occurred, try parsing once again until success
+            self.parse_main_window_content()
