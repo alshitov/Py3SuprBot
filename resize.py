c43fb4a084f552e6da48247e9ef236f02ff6f4bd
Alexander <joj.lean@yandex.ru>
2018-10-05 07:43:52 +0300

    Edited resizer(200px images for cart, 500px images for item_modal_window)

--- resize.py
+++ resize.py
@@ -1,19 +1,30 @@
 import PIL
 from PIL import Image
 import os
+import re
 
 
 def resize_images():
-    basewidth = 200
-    images_path = os.path.dirname(os.path.realpath(__file__)) + '/TempPNGS'
-    images_list = sorted(['TempPNGS/' + img for img in os.listdir(images_path)])
+    sizes = [200, 500]
+    images_path = os.path.dirname(os.path.realpath(__file__)) + '/img/'
 
-    for idx, img in enumerate(images_list):
-        img = Image.open(images_list[idx])
-        wpercent = (basewidth / float(img.size[0]))
-        hsize = int((float(img.size[1]) * float(wpercent)))
-        image = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-        image.save('TempPNGS/' + 'new' + str(0) + str(idx + 1) + '.png')
+    images_list = sorted([file[:-4] for file in os.listdir(images_path)
+                          if re.search('(.*?).jpg', file)], key=int)
 
-    for pic in images_list:
-        os.remove(pic)
+    print(images_list)
+
+    for basewidth in sizes:
+        for idx, img in enumerate(images_list):
+            img = Image.open(images_path + img + '.jpg')
+            wpercent = (basewidth / float(img.size[0]))
+            hsize = int((float(img.size[1]) * float(wpercent)))
+            image = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+
+            if basewidth == 200:
+                image.save(images_path + '200px/' + str(idx) + '.jpg')
+            elif basewidth == 500:
+                image.save(images_path + '500px/' + str(idx) + '.jpg')
+
+
+if __name__ == '__main__':
+    resize_images()
